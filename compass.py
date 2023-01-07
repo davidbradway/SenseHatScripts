b569c01e63cd144dda01d873316dcbbd255645e8
David Bradway <david.bradway@gmail.com>
2023-01-07 17:01:04 -0500

    add rotating image

--- compass.py
+++ compass.py
@@ -1,21 +1,35 @@
+import numpy as np
+from PIL import Image, ImageChops
 from sense_hat import SenseHat
 
 
 def main():
     sense = SenseHat()
-    sense.set_rotation(180)
+    sense.set_rotation(90)
+
+    # load a PNG from file
+    img = Image.open('0.png')
+
     try:
         while True:
-            direction = find_dir(sense.compass)
-            sense.show_message(direction)
+            direction = find_dir(sense.compass, img, sense)
+            #sense.show_message(direction)
+            print(direction)
     except:
+        print('\n Quitting')
         sense.clear()
 
 
-def find_dir(deg):
+def find_dir(deg, img, sense):
     directions = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW',
                   'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW', 'N']
     index = round(deg/22.5)
+    # Rotate and invert the pixel values
+    inverted = ImageChops.invert(img.rotate(angle=deg, fillcolor=(255,255,255)))
+    # Convert to Numpy array and flatten rows and columns
+    flatter = np.array(inverted).reshape(64,4)
+    # remove alpha value from RGBA and write to matrix array
+    sense.set_pixels(flatter[:, 0:3])
     return directions[index]
 
 
--- image.py
+++ image.py
@@ -9,13 +9,13 @@ sense = SenseHat()
 img = Image.open('0.png')
 
 for i in np.linspace(0,360,17):
-	# Rotate and invert the pixel values
-	inverted = ImageChops.invert(img.rotate(angle=i, fillcolor=(255,255,255)))
+    # Rotate and invert the pixel values
+    inverted = ImageChops.invert(img.rotate(angle=i, fillcolor=(255,255,255)))
 
-	# Convert to Numpy array and flatten rows and columns
-	flatter = np.array(inverted).reshape(64,4)
-	# remove alpha value from RGBA and write to matrix array
-	sense.set_pixels(flatter[:, 0:3])
-	time.sleep(1)
+    # Convert to Numpy array and flatten rows and columns
+    flatter = np.array(inverted).reshape(64,4)
+    # remove alpha value from RGBA and write to matrix array
+    sense.set_pixels(flatter[:, 0:3])
+    time.sleep(1)
 
 sense.clear()
